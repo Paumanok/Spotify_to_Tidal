0e69fa002c9115ad55ef92624c6af71b646fe9d7
matthew smith <guitarbass95@gmail.com>
2021-05-03 20:08:13 -0400

    things working a lot nicer, if you actually read this just check the diff. Oh yeah we got args now, no help menu though so gl

--- spotify_to_tidal.py
+++ spotify_to_tidal.py
@@ -6,14 +6,16 @@ import logging
 import os.path
 import time
 from spotify_auth import spotify_auth
+from tqdm import tqdm
+import argparse
 
 tidal_session_file = "tidal_session.json"
 search_results_file = "tidal_search_results.txt"
-
+verbose = False
 
 def find_most_common(tracks):
 
-    print("looking for the best match")
+    if verbose:  print("looking for the best match")
     id_tally = {}
     for query in tracks["q_results"]:
         #print("cur query: " + query["query"])
@@ -42,16 +44,16 @@ def find_most_common(tracks):
                 largest = t_id
     
     tracks["track"]["most_likely"] = largest
-    print("most likely song: " +  str(largest))
+    if verbose: print("most likely song: " +  str(largest))
 
 
 
-def get_spotify_playlist():
+def get_spotify_playlist(playlist_id):
     sa = spotify_auth()
     sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=sa.client_id,
                                                                 client_secret=sa.client_secret))
-    playlist_id = "78YKN3vgPbQc5BX1LoePb4"
-    
+    #playlist_id = "1pzTqhESG4DCxACidCTLTh"
+    print("Gathering Spotify playlist tracklist") 
     #results = sp.playlist(playlist_id,fields='tracks.items.track(name,album(name,artists(name)))', offset=0)
     offset = 0
     results = sp.playlist_items(playlist_id,fields='items.track(name,album(name,artists(name))),total', offset=offset)
@@ -62,17 +64,26 @@ def get_spotify_playlist():
         offset += 100
         results = sp.playlist_items(playlist_id,fields='items.track(name,album(name,artists(name))),total', offset=offset)
         tracks.extend(results["items"])
-        n_items += len(tracks)
-#    print(results)
+        n_items += len(results['items'])
+        if verbose: print("n_items" + str(n_items))
+    print("Total tracks in Spotify playlist: " + str(total))
    
     tracklist = []
-    for track in tracks:
-        #cur_track = track['track']['name'] + "<^>" + track['track']['album']['artists'][0]['name'] + "<^>" +  track['track']['album']['name']
-        cur_track = {"name":track['track']['name'] , "artist": track['track']['album']['artists'][0]['name'], "album": track['track']['album']['name']}
+    localtracks = 0
+    for track in tqdm(tracks):
+        try:
+            #skip local tracks added to playlist
+            cur_track = {"name":track['track']['name'] , "artist": track['track']['album']['artists'][0]['name'], "album": track['track']['album']['name']}
+        except:
+            if 'is_local' in track['track']:
+                localtracks += 1
+            #some other issue
+            continue
+
      
         tracklist.append(cur_track)
-    
-    print("Found " + str(len(tracklist)) + " Songs on spotify playlist.")
+    print("Found {0} Songs on Spotify playlist. Unable to find {1} local tracks\n".format( str(len(tracklist)), str(localtracks)))
+    #print("Found " + str(len(tracklist)) + " Songs on spotify playlist. Unable to find ")
     return tracklist
 
 def get_tidal_session():
@@ -97,15 +108,17 @@ def get_tidal_session():
     return session
 
 
-def build_json():
+def build_json(playlist_id):
     
-    tracklist = get_spotify_playlist()
+    tracklist = get_spotify_playlist(playlist_id)
     
     session = get_tidal_session()
     
     search_results = []
     
-    for track in tracklist:
+    print("Searching for track equivalents in Tidal....")
+
+    for track in tqdm(tracklist):
         #query = ' '.join(track.split('<^>')[0:2])
         #query2 = ' '.join([track.split('<^>')[0],track.split('<^>')[-1]])
         queries = [{"query" : ' '.join([track["name"]]), "models" : [tidalapi.media.Track]},
@@ -117,7 +130,7 @@ def build_json():
         q_results = []  
     
         for query in queries:
-            print("Searching for: " + query["query"])
+            #print("Searching for: " + query["query"])
             result = session.search(query["query"],models=query["models"])
             #print(result)
             result_array = []
@@ -142,7 +155,7 @@ def build_json():
         find_most_common(track)
         search_results.append(track)
     
-    print("writing results")
+    print("Writing Result File")
     with open(search_results_file, 'w') as rfp:
         json.dump(search_results, rfp, indent=4)
 
@@ -150,7 +163,7 @@ def build_json():
 def chunker(seq, size):
     return (seq[pos:pos + size] for pos in range(0, len(seq), size)) 
 
-def create_playlist():
+def create_playlist(playlist_name, playlist_description):
     search_results = []
     
     with open(search_results_file, 'r') as rfp:
@@ -160,10 +173,10 @@ def create_playlist():
     
     user = session.user
     
-    playlist = user.create_playlist("skatest", "automating the pickitup")
+    playlist = user.create_playlist(playlist_name, playlist_description)
     
     ids_to_add = []
-    for track in search_results:
+    for track in tqdm(search_results):
         if "most_likely" in track["track"]:
             m_l = track["track"]["most_likely"]
             if m_l is None:
@@ -174,24 +187,48 @@ def create_playlist():
     print("Found " + str(len(ids_to_add)) + " tracks to populate. unfound tracks may still exist on tidal!")
 
     index = 0
+    added = 0
     #print( len(ids_to_add))
     #print(len(get_spotify_playlist()))
     for group in chunker(ids_to_add, 50):
         try:
             playlist.add(group)
+            added += len(group)
         except:
             print("not working: " + str(group))
             print(index)
-        index +=1
-    
+        index +=2
+        time.sleep(1)
+    print("Playlist import complete, {0} Songs added.".format(added))
+
+#playlist_id = "1pzTqhESG4DCxACidCTLTh"
+#playlist_id = "6Ig27y0QryKLs9BWhzhfc6"
+playlist_id = ""
 #logging.basicConfig(level=logging.DEBUG)
-#build_json()
-#create_playlist()
-#print(search_results)
-get_spotify_playlist()
 
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument( "-b", "--build_json", action='store_true')
+    parser.add_argument("-c", "--create_playlist", action='store_true')
+    parser.add_argument("-id", "--playlist_id",default=playlist_id, type=str)
+    parser.add_argument("--playlist_name", type=str)
+    
+    args = parser.parse_args()
+    if args.build_json: 
+        if args.playlist_id == None:
+            print("missing playlist id... exiting")
+            exit()
+        else:
+            build_json(args.playlist_id)
 
+    if args.create_playlist:
+        if args.playlist_name == None:
+            print("missing playlist name... exiting")
+            exit()
+        else:
+            print("creating playlist")
+            create_playlist(args.playlist_name, "")
 
 
 
